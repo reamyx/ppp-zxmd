f197a90a6d526c0e75781976a23ae206417bdd73
zhixia <reamyx@126.com>
2019-06-19 00:42:09 +0800

    暂存: 常规远程同步

--- pppd/plugins/exfuncpy.c
+++ pppd/plugins/exfuncpy.c
@@ -1,7 +1,7 @@
 
 // exfuncpy.c - Hook点pyhton3转换插件
 // Copyright 2019 Reamyx Liou, Xi'an in China.
-// 本插件构造适用参数并转换pppd插件回调(包括Hook和notify)到python3过程调用
+// 本插件构造适用参数并转换pppd插件部分回调(包括Hook和notify)到python3过程调用
 // 除回调过程规定的参数外,插件还将封装其它适用的数据结构和过程到默认参数表尾部
 
 
@@ -10,10 +10,12 @@
 #include <syslog.h>
 #include <string.h>
 #include <ctype.h>
+#include <python3.7m/Python.h>
+
 #include "pppd.h"
 #include "chap-new.h"
-#include <Python.h>
 
+static void notify_callpy(void *, int);
 
 // 插件适用版本
 char pppd_version[] = VERSION;
@@ -26,135 +28,114 @@ static option_t exfuncpy_options[] = {
       OPT_STATIC, NULL, PATH_MAX },
     { NULL } };
 
+// 名称和计数
+wchar_t *PyProName = L"pppdextpy";
 
 // 命名过程指针
 static PyObject
-    *exfuncpy_init_func,
-    *idle_time_func,
-    *holdoff_func,
+    *extpy_init_func,
+    //*idle_time_func,
+    //*holdoff_func,
     *pap_check_func,
-    *pap_passwd_func,
+    //*pap_passwd_func,
     *pap_auth_func,
     *chap_check_func,
-    *chap_passwd_func,
+    //*chap_passwd_func,
     *chap_verify_func,
-    *null_auth_func,
-    *ip_choose_func,
+    //*null_auth_func,
+    //*ip_choose_func,
     *allowed_address_func,
-    *snoop_recv_func,
-    *snoop_send_func,
-    *multilink_join_func,
-    *notify_fetch_func;
+    //*snoop_recv_func,
+    //*snoop_send_func,
+    //*multilink_join_func,
+    *M, *AL;
 
 // pppd通知链"名称"映射表
-static struct{char *name, struct notifier **chain}
-ncmap[] = {
-    {"pidchange",           &pidchange          },
-    {"phasechange",         &phasechange        },
-    {"exitnotify",          &exitnotify         },
-    {"sigreceived",         &sigreceived        },
-    {"ip_up_notifier",      &ip_up_notifier     },
-    {"ip_down_notifier",    &ip_down_notifier   },
-    {"auth_up_notifier",    &auth_up_notifier   },
-    {"link_down_notifier",  &link_down_notifier },
-    {"fork_notifier",       &fork_notifier      },
-    {NULL}};
-
+static struct{
+    char              *name;
+    struct notifier  **chp;
+    PyObject          *ncb;
+    } ntmap[] = {
+        //{"ncb_pidchange",         &pidchange,          NULL},
+        //{"ncb_phasechange",       &phasechange,        NULL},
+        {"ncb_exitnotify",          &exitnotify,         NULL},
+        {"ncb_sigreceived",         &sigreceived,        NULL},
+        {"ncb_ip_up_notifier",      &ip_up_notifier,     NULL},
+        {"ncb_ip_down_notifier",    &ip_down_notifier,   NULL},
+        {"ncb_auth_up_notifier",    &auth_up_notifier,   NULL},
+        //{"ncb_link_down_notifier",&link_down_notifier, NULL},
+        //{"ncb_vfork_notifier",    &fork_notifier,      NULL},
+        {NULL}};
 
 // python环境初始化和回调导出
-static void py_env_init(void) {
-    PyObject *args;
-
+static void py_env_init() {
     // 设置环境中程序名称,初始化(反初始化: Py_Finalize();)
-    Py_SetProgramName("????"); 
-    Py_IsInitialized() ?0: Py_Initialize();
+    Py_SetProgramName(PyProName); 
+    if(!Py_IsInitialized())Py_Initialize();
     
     // 导入目标模块,需要修正模块路径参数
     M = PyImport_Import(PyString_FromString(modulepath));
     
-    // 从目标模块导出回调过程
-    exfuncpy_init_func   = PyObject_GetAttrString(M, "exfuncpy_init"  );
-    idle_time_func       = PyObject_GetAttrString(M, "idle_time"      );
-    holdoff_func         = PyObject_GetAttrString(M, "holdoff"        );
+    // 从目标模块导出通知回调过程
+    for(int i=0;ntmap[i].name;i++){
+        ntmap[i].ncb = PyObject_GetAttrString(M, ntmap[i].name);}
+    
+    // 从目标模块导出HOOK回调过程
+    //idle_time_func     = PyObject_GetAttrString(M, "idle_time"      );
+    //holdoff_func       = PyObject_GetAttrString(M, "holdoff"        );
     pap_check_func       = PyObject_GetAttrString(M, "pap_check"      );
-    pap_passwd_func      = PyObject_GetAttrString(M, "pap_passwd"     );
+    //pap_passwd_func    = PyObject_GetAttrString(M, "pap_passwd"     );
     pap_auth_func        = PyObject_GetAttrString(M, "pap_auth"       );
     chap_check_func      = PyObject_GetAttrString(M, "chap_check"     );
-    chap_passwd_func     = PyObject_GetAttrString(M, "chap_passwd"    );
+    //chap_passwd_func   = PyObject_GetAttrString(M, "chap_passwd"    );
     chap_verify_func     = PyObject_GetAttrString(M, "chap_verify"    );
-    null_auth_func       = PyObject_GetAttrString(M, "null_auth"      );
-    ip_choose_func       = PyObject_GetAttrString(M, "ip_choose"      );
+    //null_auth_func     = PyObject_GetAttrString(M, "null_auth"      );
+    //ip_choose_func     = PyObject_GetAttrString(M, "ip_choose"      );
     allowed_address_func = PyObject_GetAttrString(M, "allowed_address");
-    snoop_recv_func      = PyObject_GetAttrString(M, "snoop_recv"     );
-    snoop_send_func      = PyObject_GetAttrString(M, "snoop_send"     );
-    multilink_join_func  = PyObject_GetAttrString(M, "multilink_join" );
-    notify_fetch_func    - PyObject_GetAttrString(M, "notify_fetch"   );}
+    //snoop_recv_func    = PyObject_GetAttrString(M, "snoop_recv"     );
+    //snoop_send_func    = PyObject_GetAttrString(M, "snoop_send"     );
+    //multilink_join_func= PyObject_GetAttrString(M, "multilink_join" );
+    extpy_init_func      = PyObject_GetAttrString(M, "extpy_init"     );}
 
 // 初始化回调,传入pppd本地名称our_name和ipparm
-static void exfuncpy_init_callpy(){
+static void extpy_init_callpy(){
+    if(!extpy_init_func)return;
     PyObject *args;
-    exfuncpy_init_func ?0: return;
     args = PyTuple_New(2);
     PyTuple_SetItem(args,0, PyString_FromString(our_name));
     PyTuple_SetItem(args,1, PyString_FromString(ipparam));
-    PyObject_CallObject(exfuncpy_init_func, args); }
-
-// 通知回调: 根据注册时邦定的回调对象bondf确定具体回调方法
-static void notify_callpy(void *bondf, int arg){
+    PyObject_CallObject(extpy_init_func, args);}
 
 
-
-
-}
-
-// 通知回调注册
+// 通知注册
 static void notify_callback_reg() {
-    PyObject *res;
-    res = PyObject_CallObject(notify_fetch_func, NULL);
-    res ?0: return;
+    for(int i=0;ntmap[i].name;i++){
+    if(ntmap[i].ncb)
+    add_notifier(ntmap[i].chp, notify_callpy, (void*)(ntmap[i].ncb));}}
 
-
-
-    add_notifier(&interesting_notifier, my_notify_proc, opaque);
-
-
-
-}
-
-struct notifier {
-    struct notifier *next;
-    notify_func	    func;
-    void	    *arg;
-}
-
-
-
-// 
-static int idle_time_callpy(struct ppp_idle *idlep){
-}
-
-//
-static int holdoff_callpy(void){
-}
-
+// 通知回调: 根据注册时邦定的回调对象ncb确定具体回调方法
+static void notify_callpy(void *ncb, int IntArg){
+    PyObject *args;
+    for(int i=0;ntmap[i].name;i++){
+        if((void*)(ntmap[i].ncb)!=ncb)continue;
+        args = PyTuple_New(1);
+        PyTuple_SetItem(args,0, PyLong_FromLong(IntArg));
+        PyObject_CallObject(pap_check_func, args);
+        break;}}
 
 // 对端pap认证指示,返回: 1认证 0不认证 -1常规pap-secrets文件认证
 static int pap_check_callpy(void) {
     return pap_check_func ?
-    PyInt_AsLong(PyObject_CallObject(pap_check_func, NULL)) : -1; }
-
-// 
-static int pap_passwd_callpy(char *user, char *passwd) {
-}
+    PyLong_AsLong(PyObject_CallObject(pap_check_func, NULL)) : -1; }
 
 // pap认证检查过程 返回: 1成功 0失败 -1常规pap-secrets检查
 static int pap_auth_callpy(char *user, char *passwd,
     char **msgp, struct wordlist **paddrs, struct wordlist **popts) {
     
+    if(!pap_auth_func)return -1;
+    
     struct wordlist *wtp;
     PyObject *args, *res, *msg, *addrs, *opts;
-    
-    pap_auth_func ?0: return -1;
 
     // 构造调用参数msg列表用于回写
     msg = PyList_New(0);
@@ -179,7 +160,7 @@ static int pap_auth_callpy(char *user, char *passwd,
     PyTuple_SetItem(args,2, msg);
     PyTuple_SetItem(args,3, addrs);
     PyTuple_SetItem(args,4, opts);
-    res = PyObject_CallObject(pap_auth_func, args) 
+    res = PyObject_CallObject(pap_auth_func, args);
 
     // 回写字符串列表msg,addrs和opts在目标过程中的变更
     
@@ -189,11 +170,7 @@ static int pap_auth_callpy(char *user, char *passwd,
 // 对端chap认证指示,返回: 1认证 0不认证 -1常规chap-secrets文件认证
 static int chap_check_callpy(void) {
     return chap_check_func ?
-    PyInt_AsLong(PyObject_CallObject(chap_check_func, NULL)) : -1; }
-    
-// 
-static int chap_passwd_callpy(char *user, char *passwd) {
-}
+    PyLong_AsLong(PyObject_CallObject(chap_check_func, NULL)) : -1; }
 
 // chap认证检查过程 返回: 1成功 0失败
 static int chap_verify_callpy(char *user, char *ourname,
@@ -204,37 +181,12 @@ static int chap_verify_callpy(char *user, char *ourname,
         id, user, pwbuff, strlen(pwbuff),
         challenge, response, message, message_space);}
 
-//
-static int null_auth_callpy(
-    struct wordlist **paddrs, struct wordlist **popts){
-}
-
-//
-static void ip_choose_callpy(u_int32_t *addrp){
-}
-
 // 返回对端地址确认: 1允许 0拒绝 -1常规处理
 static int allowed_address_callpy(unsigned int addr) {
     PyObject *args; args = PyTuple_New(1);
     PyTuple_SetItem(args,0, PyInt_FromLong(addr));
     return allowed_address_func ?
     PyInt_AsLong(PyObject_CallObject(allowed_address_func, args)) : -1; }
-
-//
-static void (*snoop_recv_hook)(unsigned char *p, int len){
-
-}
-
-//
-static void (*snoop_send_hook)(unsigned char *p, int len){
-
-}
-
-//
-static void (*multilink_join_hook)(){
-
-}
-
 
 
 // 插件: 功能注册
@@ -243,26 +195,27 @@ void plugin_init(void) {
     add_options(exfuncpy_options);
     
     // (这里需要执行选项解析)
-
+    
     // 环境初始化:
     py_env_init();
-    exfuncpy_init_callpy();
+    extpy_init_callpy();
     
     // 通知注册:
     notify_callback_reg();
 
     // Hook注册:
-    idle_time_func       ? idle_time_hook=idle_time_callpy             :0;
-    holdoff_func         ? holdoff_hook=holdoff_callpy                 :0;
-    pap_check_func       ? pap_check_hook=pap_check_callpy             :0;
-    pap_passwd_func      ? pap_passwd_hook=pap_passwd_callpy           :0;
-    pap_auth_func        ? pap_auth_hook=pap_auth_callpy               :0;
-    chap_check_func      ? chap_check_hook=chap_check_callpy           :0;
-    chap_passwd_func     ? chap_passwd_hook=chap_passwd_callpy         :0;
-    chap_verify_func     ? chap_verify_hook=chap_verify_callpy         :0;
-    null_auth_func       ? null_auth_hook=null_auth_callpy             :0;
-    ip_choose_func       ? ip_choose_hook=ip_choose_callpy             :0;
-    allowed_address_func ? allowed_address_hook=allowed_address_callpy :0;
-    snoop_recv_func      ? snoop_recv_hook=snoop_recv_callpy           :0;
-    snoop_send_func      ? snoop_send_hook=snoop_send_callpy           :0;
-    multilink_join_func  ? multilink_join_hook=multilink_join_callpy   :0; }
+    //idle_time_func      ? idle_time_hook=idle_time_callpy             :0;
+    //holdoff_func        ? holdoff_hook=holdoff_callpy                 :0;
+    pap_check_func        ? pap_check_hook=pap_check_callpy             :0;
+    //pap_passwd_func     ? pap_passwd_hook=pap_passwd_callpy           :0;
+    pap_auth_func         ? pap_auth_hook=pap_auth_callpy               :0;
+    chap_check_func       ? chap_check_hook=chap_check_callpy           :0;
+    //chap_passwd_func    ? chap_passwd_hook=chap_passwd_callpy         :0;
+    chap_verify_func      ? chap_verify_hook=chap_verify_callpy         :0;
+    //null_auth_func      ? null_auth_hook=null_auth_callpy             :0;
+    //ip_choose_func      ? ip_choose_hook=ip_choose_callpy             :0;
+    allowed_address_func  ? allowed_address_hook=allowed_address_callpy :0;
+    //snoop_recv_func     ? snoop_recv_hook=snoop_recv_callpy           :0;
+    //snoop_send_func     ? snoop_send_hook=snoop_send_callpy           :0;
+    //multilink_join_func ? multilink_join_hook=multilink_join_callpy   :0;
+    }
--- pppd/plugins/explugin.py
+++ pppd/plugins/explugin.py
@@ -6,13 +6,13 @@
 #
 # exfuncpy是一个pppd插件程序,它运行时导入目标模块并转换pppd插件每一个回调(包括
 # Hook和notify)到该模块中对应的命名过程,即当前模块中特定名称的可调用对象.
-# 
+# (当前仅实现部分必要的HOOK和通知回调用于对端认证).
+#
 # 特定命名过程的缺失将将使忽略对应的插件回调操作.
 # 除回调过程规定的参数外,插件还可能封装其它适用的数据结构和过程到默认参数表尾部.
 #
 # 目标模块文件的路径名称通过exfuncpy插件提供的pppd选项"pymodulepath"指定.
 #--------------------------------------------------------------------
-
 
 import os, sys
 
@@ -22,30 +22,30 @@ if __name__ == '__main__':
     sys.exit(0)
 
 #插件初始化完成指示,无返回值
-def exfuncpy_init(ourname,ipparm)
+def extpy_init(ourname,ipparm):
     pass
+
 
 ############################## 通知回调 ##############################
 #事件通知类型:
-# pidchange, phasechange, exitnotify, sigreceived
-# ip_up_notifier, ip_down_notifier, auth_up_notifier
-# link_down_notifier
+# pidchange, phasechange, exitnotify, sigreceived, ip_up_notifier,
+# ip_down_notifier, auth_up_notifier, link_down_notifier
 #
 #事件回调过程: 无返回值,整数IntArg值和通知类型相关,可选参数取决于具体的事件类型
-# def notify_callback_f00(IntArg, ...)
+# def ncb_<event-class-name>(IntArg)
 
 #ip_up_notifier: 
-def notify_ip_up_notifier(IntArg, ...)
+def ncb_ip_up_notifier(IntArg):
     pass
 
 #ip_down_notifier: 
-def notify_ip_down_notifier(IntArg, ...)
+def ncb_ip_down_notifier(IntArg):
     pass
 
 
 #通知事件项拉取,返回元组表示的的事件表用于注册到通告回调.
 #其元素为一个二元组,表示一个注册项: ((事件类型, 回调过程), ...)
-def notify_fetch()
+def notify_fetch():
     return (
     ("ip_up_notifier", notify_ip_up_notifier),
     ("ip_down_notifier", notify_ip_down_notifier))
@@ -57,24 +57,24 @@ def notify_fetch()
 ############################## Hook回调 ##############################
 
 #对端pap认证指示 1认证 0不认证 -1常规pap-secrets文件认证
-def pap_check()
+def pap_check():
     pass
 
 #pap认证过程 返回值: 1成功 0失败 -1常规pap-secrets检查
 # user, passwd:字符串,msgp: 字符串列表
-def pap_auth(user, passwd, msgp, paddrs, popts)
+def pap_auth(user, passwd, msgp, paddrs, popts):
     pass
 
 #对端chap认证指示 1认证 0不认证 -1常规chap-secrets文件认证
-def chap_check()
+def chap_check():
     pass
 
 #chap认证过程 返回值: 1成功 0失败
 def chap_verify(user, ourname, id, digest, 
-    challenge, response, message, message_space)
+    challenge, response, message, message_space):
     pass
 
 #对端地址确认: 1允许 0拒绝 -1常规处理
 # addr: INT表示的IP地址
-def allowed_address(addr)
+def allowed_address(addr):
     return 1
